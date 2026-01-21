9298ba0793bdd75b634174047b48098427220fed
blazeviper928 <241159@iiitt.ac.in>
2026-01-21 14:15:31 +0530

    Delete counter.py

--- counter.py
+++
@@ -1,9 +0,0 @@
-import time
-my_time=int(input("Enter the time in seconds:"))
-for x in range(my_time,0,-1):
-    minutes=x//60
-    seconds=x%60
-    print(f"00:{minutes:02}:{seconds:02}")
-    time.sleep(1)
-print("TIME IS UP!")    
-
